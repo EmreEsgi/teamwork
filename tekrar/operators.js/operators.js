e15495b51079bb5cd6ab3f5a4e022c24dfb6496e
EmreEsgi <esgiemre9@gmail.com>
2024-11-03 21:31:22 +0300

    first commit

---
+++ tekrar/operators.js/operators.js
@@ -0,0 +1,22 @@
+
+
+let a=10
+let b=2
+
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a** 100)
+console.log(a % b)
+
+
+
+
+let sure=377
+let saat=Math.floor(sure/60);
+console.log(saat);
+let dakika=sure%60;
+console.log(dakika)
+console.log(`${saat}:${dakika}`)
